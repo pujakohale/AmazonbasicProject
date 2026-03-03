65768b63628a19ad1e62851698af1246cbdd51fc
puja kohale <kohalepuja7@gmail.com>
2026-03-03 16:48:19 +0530

    Initial commit:amazon maven project

---
+++ src/test/java/com/amazon/amazon_project/AmazonTest.java
@@ -0,0 +1,22 @@
+package com.amazon.amazon_project;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class AmazonTest {
+
+	    @Test
+	    public void openTest() throws InterruptedException {
+
+	        WebDriver driver = new ChromeDriver();
+	        driver.get("https://www.amazon.com/");
+            driver.manage().window().maximize();
+            Thread.sleep(3000);
+	        System.out.println("Title: " + driver.getTitle());
+
+	        driver.quit();
+	    }
+}
+
